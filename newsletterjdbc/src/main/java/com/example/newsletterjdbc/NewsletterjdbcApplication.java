18b22e13e2cd8a109ec814eafa4c2e96938f1b0f
HenriqueArroyo <ana123ca.henrique@gmail.com>
2024-03-07 14:51:57 -0300

    07/03

---
+++ newsletterjdbc/src/main/java/com/example/newsletterjdbc/NewsletterjdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsletterjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsletterjdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsletterjdbcApplication.class, args);
+	}
+
+}
